ff056bf6c6a1b5ea671a8875a927a859d5216253
Olivia Chang <hello@oliviachang.me>
2018-07-21 18:47:39 -0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+Barba.Pjax.start();
+
+var HideShowTransition = Barba.BaseTransition.extend({
+  start: function() {
+    this.newContainerLoading.then(this.finish.bind(this));
+  },
+
+  finish: function() {
+    document.body.scrollTop = 0;
+    this.done();
+  }
+});
+
+Barba.Pjax.getTransition = function() {
+  return HideShowTransition;
+};
+
+$(function() {
+  Barba.Pjax.start();
+});
